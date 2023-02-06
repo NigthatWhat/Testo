b4171cd42ddb3334210c5c5d7a9c236236f78a51
Dmitriy <roldugindima4@gmail.com>
2023-02-06 21:08:35 +0300

    first commit

---
+++ src/Laba1/Niam.java
@@ -0,0 +1,9 @@
+package Laba1;
+
+public  class Niam {
+    public static void main(String[] args) {
+      int a = 10;
+      int b=20;
+        System.out.println("Cумма чисел "+(a + b));
+    }
+}
